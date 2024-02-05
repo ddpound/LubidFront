3a2826a77103f7a97cd76c5e8a9770e334294570
dpound@naver.com <dpound@naver.com>
2024-02-05 21:21:32 +0900

    서버 연결을 위한 준비중

--- screens/test.js
+++ screens/test.js
@@ -1,14 +1,33 @@
+import axios from "axios";
 import {React, useEffect, useState} from "react";
 import { View, Text } from "react-native";
 import { Button } from "react-native";
+
+ //바코드 번호로 품목보고번호 얻기
+ const getRepotNo = async () => {
+  const response = await fetch(
+    'http://localhost:7001/auth/test/welcome',
+    {
+      method: 'GET',
+    },
+  );
+
+  if (response.status === 200) {
+    const responseJson = await response.json();
+    return responseJson.C005.row[0];
+  } else {
+    return 0;
+    // throw new Error('unable to get');
+  }
+};
 
 function Test({navigation}) {
   const [testText, setTestText] = useState('');
   
   useEffect(()=>{
     setTestText('hi');
+    getRepotNo();
   },[])
-  
   return ( 
     <View>
       <Text>{testText}</Text>
